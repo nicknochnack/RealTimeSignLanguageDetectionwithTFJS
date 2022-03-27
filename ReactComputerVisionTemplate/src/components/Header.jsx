8aeff014f5833f732b2f004dc21ab14ec5cd8ee4
ZKaptan <dkkaptan20@gmail.com>
2022-03-27 20:04:31 -0300

    Styling changes

---
+++ ReactComputerVisionTemplate/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<h1>Real Time Sign Language Detection </h1>
+			<h3>Perform your action in the webcam!</h3>
+		</div>
+	);
+};
+
+export default Header;
